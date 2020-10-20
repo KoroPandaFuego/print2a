40963296400f4687917f2fd1d32b268a8341c48d
MSFTserver <msftserver@gmail.com>
2020-10-19 19:38:31 -0700

    update list.js

--- docs/list.js
+++ docs/list.js
@@ -4,15 +4,14 @@ const fs = require('fs');
 const path = require('path');
 
 const gitIgnore = fs.readFileSync('.gitignore', 'utf-8');
-const readmeRegex = new RegExp(/readme.(md|txt)$/, 'gi');
+const readmeRegex = new RegExp(/readme.md$/, 'gi');
 const imagesRegex = new RegExp(/.jpg|.png$/, 'gi');
 
 const filesAndFoldersToExclude = [
   '.git',
-  //'docs',
+  'docs',
   '.gitignore',
   '.gitattributes',
-  'node_modules',
   // remove everything listed in .gitignore
   ...gitIgnore.split('\n').filter(l => l !== '')
 ];
@@ -48,17 +47,12 @@ const infoList = thingsList.map(readmeFilepath => {
   const folder = readmeFilepath.replace(readmeRegex, '');
   const tags = folder.split('/').map(str => str.split('_')).flat().filter(str => str !== '');
   const readmeStat = fs.statSync(readmeFilepath);
-  if(readmeFilepath == 'readme.md'){return};
-	console.log('logged:'+readmeFilepath);
-	console.log('logged:'+folder);
-	//console.log('logged:'+tags);
-	console.log('logged:'+readmeStat);
 
   return {
     location: folder.replace(/\/$/, ''), //remove the last slash
     datetime: readmeStat.mtime,
     tags: tags,
-    readme: readmeFilepath ? getFilesByRegex(folder,readmeRegex) : null,
+    readme: readmeFilepath,
     renders: folder ? getFilesByRegex(folder,imagesRegex) : null
   }
 });
